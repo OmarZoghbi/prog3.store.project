4a65bb8db7a1d9a8946b592afd26ec1ea1934d6a
OmarZoghbi <omarzoghbi93@gmail.com>
2025-06-04 16:27:53 +0200

    Frontend (Vue)

---
+++ Documents/webTech2025/webtech/webtech/frontend/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Mein Online-Shop</h1>
+  <ArticleList />
+</template>
+
+<script setup lang="ts">
+import ArticleList from './components/ArticleList.vue';
+</script>
---
+++ Documents/webTech2025/webtech/webtech/frontend/src/components/ArticleList.vue
@@ -0,0 +1,11 @@
+<template>
+  <ul>
+    <li v-for="(item, index) in items" :key="index">
+      {{ item }}
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+const items = ['Alo-Vera-Gel', 'Kokosöl-Haarmaske', 'Lavendelseife', 'Bartöl'];
+</script>
